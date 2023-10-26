fdcefa5a10a46a16b9ae22ebc0551376da230722
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2023-10-27 00:24:20 +0900

    add security test

--- service1/src/test/kotlin/com/example/security/SecurityApplicationTests.kt
+++ service1/src/test/kotlin/com/example/security/SecurityApplicationTests.kt
@@ -1,13 +1,28 @@
 package com.example.security
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@SpringBootTest
+
+
+@WebMvcTest
 class SecurityApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun testGetRequest() {
+        mockMvc.perform(get("/sample"))
+            .andDo(print())
+            .andExpect(status().isForbidden)
+
+    }
 
 }
