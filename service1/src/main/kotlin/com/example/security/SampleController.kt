84aaf651f92627cc39deaceeab89a05569766f16
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2023-10-28 00:04:57 +0900

    add hasRole with ip test

--- service1/src/main/kotlin/com/example/security/SampleController.kt
+++ service1/src/main/kotlin/com/example/security/SampleController.kt
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+
 class SampleController {
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/sample")
     fun sample(): String {
         return "sample"
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/sample2")
     fun sample2(): String {
         return "sample2"
--- service1/src/main/kotlin/com/example/security/SecurityConfig.kt
+++ service1/src/main/kotlin/com/example/security/SecurityConfig.kt
@@ -8,14 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true) // 메소드보호 호출
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http
             .csrf().disable().authorizeRequests()
-            .antMatchers("/sample3").access("hasRole('ROLE_ADMIN') or hasIpAddress('localhost')")
-            .anyRequest().access("hasRole('ROLE_ADMIN')")
+            .antMatchers("/sample3").access("hasRole('ROLE_ADMIN') or hasIpAddress('127.0.0.1')")
 
     }
 }
