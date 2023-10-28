79a311ef753a23861c68f7567f56a1d2294b8a48
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2023-10-28 21:20:37 +0900

    update use annotation value

--- src/main/kotlin/com/example/security/AuthHolder.kt
+++ src/main/kotlin/com/example/security/AuthHolder.kt
@@ -1,11 +1,12 @@
 package com.example.security
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletRequestAttributes
+import org.springframework.web.method.HandlerMethod
+import org.springframework.web.servlet.HandlerMapping
 import javax.servlet.http.HttpServletRequest
 
 
@@ -16,27 +17,23 @@ class AuthHolder {
     private val ips: String? = null
 
     fun getSomeRole(): Boolean {
+        val request = (RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes).request
+        val allowedRoles = getAllowedRoles(request)
+
         val authorities = SecurityContextHolder.getContext().authentication?.authorities ?: emptyList()
-        for (auth in authorities) {
-            if (auth.authority.equals("ROLE_ADMIN")) {
-                return true
-            }
-        }
+        val isAuthenticated = authorities.any { it.authority in allowedRoles }
 
         // 현재 요청의 IP 주소를 가져오기 위해 RequestContextHolder 사용
-        val requestAttributes = RequestContextHolder.currentRequestAttributes()
-        val ipSplits = this.ips?.split(",") ?: emptyList()
+        val ip = request.remoteAddr
+        val ipSplits = ips?.split(",") ?: emptyList()
+        val isIpAddressAllowed = ip in ipSplits
 
-        if (requestAttributes is ServletRequestAttributes) {
-            val ip = requestAttributes.request.remoteAddr
-            for (allowedIp in ipSplits) {
-                if (ip == allowedIp) {
-                    return true
-                }
-            }
-        }
+        return isAuthenticated || isIpAddressAllowed
+    }
 
-        return false
-
+    fun getAllowedRoles(httpServletRequest: HttpServletRequest): Array<String> {
+        val attribute = httpServletRequest.getAttribute(HandlerMapping.BEST_MATCHING_HANDLER_ATTRIBUTE) as HandlerMethod
+        val methodAnnotation = attribute.getMethodAnnotation(SecuredOrLocalhost::class.java)
+        return methodAnnotation?.roles ?: emptyArray()
     }
 }
